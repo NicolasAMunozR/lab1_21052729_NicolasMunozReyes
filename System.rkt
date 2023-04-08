879ae8e5bb68f22fbce5d465c55291eecd2042f8
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-04-07 23:01:10 -0400

    System.rkt
    System modificado, agregue mas parametros para no quedar estrecho al trabajar

--- System.rkt
+++ System.rkt
@@ -1,5 +1,13 @@
 #lang racket
 
-(define (system name)(make-system (cons name null) null null))
+(define (system name)(make-system (cons name null) null null null))
 
-(define (make-system name drive user) (list name drive user))
+(define (make-system name drive user carpetas) (list name drive user carpetas))
+
+(define get-name car)
+
+(define get-drive cadr)
+
+(define get-user caddr)
+
+(define get-carpetas cadddr)
