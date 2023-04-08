9faf43744bd30b5ff8d174e9572c2961b1cebb51
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-04-07 23:02:57 -0400

    Add-drive.rkt
    Add-drive modificado con respecto a la modificacion del system

--- Add-drive.rkt
+++ Add-drive.rkt
@@ -1,19 +1,13 @@
 #lang racket
 
-(define get-name car)
-
-(define get-drive cadr)
-
-(define get-user caddr)
-
 (define (make-drive letra nombre capacidad)(list letra nombre capacidad))
 
-(define add-drive (lambda(system)
-                    (lambda(letra nombre capacidad)
-                      (if(null? (list-ref system 1))
-                         (make-system (get-name system)(cons(make-drive letra nombre capacidad)(get-drive system))
-                                      (get-user system))
-                         (if(member letra (flatten(list-ref system 1)))
-                            (make-system(get-name system)(get-drive system)(get-user system))
-                            (make-system (get-name system)(flatten(cons(get-drive system)(make-drive letra nombre capacidad)))
-                                         (get-user system)))))))
+(define add-drive(lambda(system)
+                   (lambda(letra nombre capacidad)
+                     (if(null? (list-ref system 1))
+                        (make-system (get-name system)(flatten(cons(make-drive letra nombre capacidad)(get-drive system)))
+                                     (get-user system)(get-carpetas system))
+                        (if(member letra (flatten(list-ref system 1)))
+                           (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system))
+                           (make-system (get-name system)(flatten(cons(get-drive system)(make-drive letra nombre capacidad)))
+                                        (get-user system)(get-carpetas system)))))))
