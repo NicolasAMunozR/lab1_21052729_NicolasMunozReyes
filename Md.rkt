10b1a26d13d59a735860e7c272e8ac924f469e82
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-04-12 01:03:06 -0400

    Md.rkt
    Md modificado con respecto a las nuevas funciones implementadas y la nueva estructura del system

--- Md.rkt
+++ Md.rkt
@@ -4,11 +4,22 @@
              (lambda(nam)
                (if(null? (list-ref system 3))
                   (make-system(get-name system)(get-drive system)(get-user system)
-                              (flatten(cons(get-carpetas system)
-                                           (string-append(list-ref(list-ref system 0)2)nam"/"))))
-                  (if(member(string-append(list-ref(list-ref system 0)2)nam"/")
-                            (flatten(list-ref system 3)))
-                     (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system))
+                              (list(append (list (first (string->list (list-ref(list-ref system 0)2)))
+                                                 (string-append(list-ref(list-ref system 0)2)nam"/") (list "null"))
+                                           (list (string-append(number->string(date-day (seconds->date(current-seconds)))) "/"
+                                                               (number->string(date-month (seconds->date(current-seconds)))) "/"
+                                                               (number->string(date-year (seconds->date(current-seconds)))) " "
+                                                               (number->string(date-hour (seconds->date(current-seconds)))) ":"
+                                                               (number->string(date-minute (seconds->date(current-seconds)))))
+                                                 (list-ref(list-ref system 0)1))))(get-papelera system)(get-fecha system))
+                  (if(member(string-append(list-ref(list-ref system 0)2)nam"/")(flatten(list-ref system 3)))
+                     (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system)(get-papelera system)(get-fecha system))
                      (make-system(get-name system)(get-drive system)(get-user system)
-                                 (flatten(cons(get-carpetas system)
-                                              (string-append(list-ref(list-ref system 0)2)nam"/")))))))))
+                                 (append (list-ref system 3)(list (append(list (first (string->list (list-ref(list-ref system 0)2)))
+                                                                               (string-append(list-ref(list-ref system 0)2)nam"/") (list "null"))
+                                                                         (list (string-append(number->string(date-day (seconds->date(current-seconds)))) "/"
+                                                                                             (number->string(date-month (seconds->date(current-seconds)))) "/"
+                                                                                             (number->string(date-year (seconds->date(current-seconds)))) " "
+                                                                                             (number->string(date-hour (seconds->date(current-seconds)))) ":"
+                                                                                             (number->string(date-minute (seconds->date(current-seconds)))))
+                                                                               (list-ref(list-ref system 0)1)))))(get-papelera system)(get-fecha system)))))))
