80ffe023112cf3fe628f017ddc7a566ce4a1dbf6
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-04-12 00:59:20 -0400

    Login.rkt
    Login modificado con respecto a la nueva estructura del system

--- Login.rkt
+++ Login.rkt
@@ -1,10 +1,9 @@
 #lang racket
 
-(define login (lambda(system)
-                (lambda(userN)
-                  (if(not(null?(cdr(list-ref system 0))))
-                     (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system))
-                     (if(and(member userN (get-user system))(not(member userN (get-name system))))
-                        (make-system(flatten(cons(get-name system)(make-user userN)))(get-drive system)
-                                    (get-user system)(get-carpetas system))
-                        (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system)))))))
+(define login (lambda(system)(lambda(userN)
+                            (if(not(null?(cdr(list-ref system 0))))
+                               (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system)(get-papelera system)(get-fecha system))
+                               (if(and(member userN (get-user system))(not(member userN (get-name system))))
+                                  (make-system(flatten(cons(get-name system)(make-user userN)))(get-drive system)
+                                              (get-user system)(get-carpetas system)(get-papelera system)(get-fecha system))
+                                  (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system)(get-papelera system)(get-fecha system)))))))
