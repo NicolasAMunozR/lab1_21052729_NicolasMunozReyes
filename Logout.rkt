8db95df8573bfda2352c81817b3a43b6e5b7eba0
nicoxd104 <nicolas.munoz.r@usach.cl>
2023-04-12 01:00:25 -0400

    Logout.rkt
    Logout modificado con respecto a la nueva estructura del system y a las nuevas funciones implementadas

--- Logout.rkt
+++ Logout.rkt
@@ -1,7 +1,11 @@
 #lang racket
 
 (define logout (lambda (system)
-                 (if(null?(cdr(get-name system)))
-                    (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system))
-                    (make-system(cdr(cons(get-name system)(remove(list-ref(list-ref system 0)1)(get-name system))))
-                                (get-drive system)(get-user system)(get-carpetas system)))))
+                 (if(equal? (length(get-name system)) 3)
+                    (make-system(cons (list-ref(list-ref system 0)0)null)
+                                (get-drive system)(get-user system)(get-carpetas system)(get-papelera system)(get-fecha system))
+                    (if(equal? (length(get-name system)) 2)
+                       (make-system(cons (list-ref(list-ref system 0)0)null)
+                                   (get-drive system)(get-user system)(get-carpetas system)(get-papelera system)(get-fecha system))
+                       (make-system(get-name system)(get-drive system)(get-user system)(get-carpetas system)(get-papelera system)(get-fecha system))))))
+
